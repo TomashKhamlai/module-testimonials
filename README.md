57785fafe13c509a8a0ddb141bb8d8d55d85570d
Tomash Khamlai <tomash.khamlai@gmail.com>
2021-04-19 06:21:51 +0700

    Create an empty module
    Signed-off-by: Tomash Khamlai <tomash.khamlai@gmail.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+# Overdose_Testimonials
---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Overdose_Testimonials',
+    __DIR__
+);
